12e0f41b76ca5c8edfb20136bd40a8ea47f33703
Ankitgif <ankitsinha2303@gmail.com>
2020-05-23 13:45:59 +0530

    [Ankit] Added function to checkMatch

--- TicTacToeBl.js
+++ TicTacToeBl.js
@@ -8,21 +8,26 @@ let coloumn;
 let input1;
 let index;
 
-
+//reset the game board
 reset = () => {
-    array = ["-", "-", "-", "-", "-", "-","-", "-", "-" ];
+    array = ["-", "-", "-", "-", "-", "-", "-", "-", "-"];
     gameStatus = 1;
     player = 1;
 }
 //print the game board
-print = () => {                                         
+print = () => {
     console.log("r/c  0 1 2");
     console.log("0", array[0], array[1], array[2]);
     console.log("1", array[3], array[4], array[5]);
     console.log("2", array[6], array[7], array[8]);
 }
+//check row and coloumn if empty or symbol is equal or not
+checkMatch = (num1, num2, num3) => {
+    if (array[num1] != "-" && array[num1] == array[num2] && array[num2] == array[num3])
+        gameStatus = 0;
+}
 //check row and coloumn with same value "X" or "O"
-checkGame = () =>{
+checkGame = () => {
     checkMatch(0, 1, 2);
     checkMatch(3, 4, 5);
     checkMatch(6, 7, 8);
@@ -39,59 +44,66 @@ computer = () => {
     coloumn = Math.random() * 3 | 0;
 }
 //set the value in board
-set = (row, coloumn, symbol) =>{
+set = (row, coloumn, symbol) => {
     index = row * 3 + coloumn;
-    if(array[index] == "-"){            //if index value is empty
+    if (array[index] == "-") {            //if index value is empty
         array[index] = symbol;          //set symbol
         player = player % 2 + 1;        //changing player
     }
-    else{
+    else {
         console.log("Position is occupied")
     }
 }
 tictactoe = () => {             //function to validate game
     reset();                    //reset the game
 
-    while(true){                   //infinite loop 
+    while (true) {                   //infinite loop 
         console.log();
-        if(player == 1){
+        if (player == 1) {
             symbol = "O";               //symbol for computer
-            console.log("Computer turns : ",symbol);
+            console.log("Computer turns : ", symbol);
         }
-        else{
+        else {
             symbol = "X";               //symbol for player
-            console.log("Player turn : ",symbol);
+            console.log("Player turn : ", symbol);
         }
         print();                            //print the game board
         console.log();
         console.log("1.For set row and coloumn ");
         console.log("2.For Restart ");
 
-        while(true){                        //infinite loop
-            if(player == 1){
+        while (true) {                        //infinite loop
+            if (player == 1) {
                 computer();                 //function call computer for row and coloumn value
                 input1 = 1;
             }
-            else if(player == 2){
+            else if (player == 2) {
                 input1 = input.questionInt("Enter 1 or 2 : ");          //input from the user
                 row = input.questionInt("Enter row value : ");          //row value
                 coloumn = input.questionInt("Enter coloumn value: ");   //coloumn value
             }
-            if(input1 == 1){
+            if (input1 == 1) {
                 set(row, coloumn, symbol); //function call set for putting the value in board
                 break;
             }
-            else if(input1 == 2){
+            else if (input1 == 2) {
                 reset();                    //for resetting the game
                 break;
             }
-            else{
+            else {
                 console.log("Enter 1 or 2");
             }
 
         }
         checkGame();
 
-
+        if (gameStatus != 1) {
+            print();
+            console.log("Game Over");
+            player = player % 2 + 1;
+            console.log("Player", player, symbol, "Wins");
+            let newGame = input.questionInt("Enter 1 for new match : ");
+            newGame == 1 ? reset() : console.log();
+        }
     }
 }
